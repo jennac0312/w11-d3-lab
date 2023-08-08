1b14dc5633a509a37b71b8bc8863a1724521095f
Jenna C <jennac0312@gmail.com>
2023-08-08 15:48:59 -0400

    fixed tip calculations

--- index.js
+++ index.js
@@ -14,13 +14,22 @@ app.get('/greeting', ( req, res ) => {
     res.send(`<h1>Hello, stranger</h1>`)
 })
 
-app.get('/tip/:total/:tipPercentage', ( req, res ) => {
-    res.send(`<h1> Expected tip is: ${(req.params.total * .20) } </h1>`)
-} )
-
 app.get(`/greeting/:name`, ( req, res ) => {
     res.send(`<h1> What's up ${req.params.name}! Its so great to see you </h1>`)
 })
+
+app.get('/tip/:total/:tipPercentage', ( req, res ) => {
+    res.send(`<h1> Expected tip is: ${(req.params.total * (req.params.tipPercentage / 100)) } </h1>`)
+} )
+
+app.get('/magic', ( req, res ) => {
+
+})
+
+
+
+
+
 
 app.listen(PORT, ( req, res ) => {
     console.log(`Server is running on port ${PORT}!! Go catch it`)
